29bd56eece99882b97d6fbd1d00f3a17edba5420
LucianoFraga27 <luciano.lucas@bllc.com.br>
2022-07-26 12:59:53 -0300

    Single render

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -16,19 +16,18 @@ class HomePageState extends State<HomePage> {
         appBar: AppBar(
           title: Text('Curso Flutterando'),
         ),
-        body: Center(
-          child: GestureDetector(
-            child: Text(
-              'Contador: $counter',
-              style: TextStyle(fontSize: 32),
-            ),
-            onTap: () {
-              setState(() {
-                counter++;
-              });
-            },
-          ),
-        ),
+        body: Container(
+          height: 200,
+          width: 200,
+          color: Colors.red,
+          child: Align(
+            alignment: Alignment.center,
+            child: Container(
+              height: 100,
+              width: 100,
+              color: Colors.blue,
+            )
+           )),
         floatingActionButton: FloatingActionButton(
           child: Icon(Icons.add),
           onPressed: () {
