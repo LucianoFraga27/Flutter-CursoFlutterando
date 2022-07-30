006873c31a04c1fafba0f238ead0017ab3283fc0
LucianoFraga27 <luciano.lucas@bllc.com.br>
2022-07-29 22:30:11 -0300

    Stack

--- lib/login_pag.dart
+++ lib/login_pag.dart
@@ -16,59 +16,75 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SizedBox(
-          width: MediaQuery.of(context).size.width,
-          height: MediaQuery.of(context).size.height,
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Container(
-                  height: 100,
-                  width: 100,
-                  child: Image.asset("assets/images/logo.png"),
-                ),
-                Container(height: 40),
-                TextField(
-                  onChanged: (text){
-                    email = text;
-                  },
-                  keyboardType: TextInputType.emailAddress,
-                  decoration: InputDecoration(
-                      labelText: "Email",
-                      border: OutlineInputBorder()
-                  ),
-                ),
-                Container(height: 10),
-                TextField(
-                  onChanged: (text){
-                    password = text;
-                  },
-                  obscureText: true,
-                  decoration: InputDecoration(
-                      labelText: "Password",
-                      border: OutlineInputBorder()
-                  ),
-                ),
-                Container(height: 10),
-                RaisedButton(onPressed: () {
-                  if (email == 'lc@gl.com' && password == '123'){
-                    print("correto");
-                    Navigator.of(context).pushNamed('/home');
-                    //push Named
-                    // push Replacement Named
-                  } else {
-                    print("incorreto");
-                  }
+      body: Stack(
+        children: [
+          SizedBox(
+            height: MediaQuery.of(context).size.height,
+            child: Image.asset("assets/images/background.jpg",
+            fit: BoxFit.cover),
+          ),
+          Container(
+            color: Colors.black.withOpacity(0.35)
+          ),
+          _body(context),
+        ],
+      ),
+      );
 
+  }
+
+  SizedBox _body(BuildContext context) {
+    return SizedBox(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                height: 150,
+                width: 150,
+                child: Image.asset("assets/images/logo.png"),
+              ),
+              Container(height: 40),
+              TextField(
+                onChanged: (text){
+                  email = text;
                 },
-                child: Text("Entrar!"))
-              ],
-            ),
+                keyboardType: TextInputType.emailAddress,
+                decoration: InputDecoration(
+                    labelText: "Email",
+                    border: OutlineInputBorder()
+                ),
+              ),
+              Container(height: 10),
+              TextField(
+                onChanged: (text){
+                  password = text;
+                },
+                obscureText: true,
+                decoration: InputDecoration(
+                    labelText: "Password",
+                    border: OutlineInputBorder()
+                ),
+              ),
+              Container(height: 10),
+              RaisedButton(onPressed: () {
+                if (email == '@' && password == '123'){
+                  print("correto");
+                  Navigator.of(context).pushNamed('/home');
+                  //push Named
+                  // push Replacement Named
+                } else {
+                  print("incorreto");
+                }
+
+              },
+              child: Text("Entrar!"))
+            ],
           ),
         ),
       );
-
   }
 }
